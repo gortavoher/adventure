40a753853866e1378834d66fcd0c3e8a1a7184c7
Jeremiah Russell <jerry@jrussell.ie>
2023-09-14 07:22:30 +0100

    Hello people (components and startup)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-use bevy::prelude::{App, Component, Update};
+use bevy::prelude::{App, Commands, Component, Query, Startup, Update, With};
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
 
 fn hello_world() {
-    println!("hello world");
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person, Name("Jerry Russell".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
